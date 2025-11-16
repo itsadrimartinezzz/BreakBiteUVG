515b7362cd919204c2b87ddd3400e37c31c3cd66
Chon211205 <lee241203@uvg.edu.gt>
2025-11-15 22:21:24 -0600

    documentacion de LocalAllItems

--- app/src/main/java/com/martinezzf/breakbitee/ui/navegation/LocalAllItems.kt
+++ app/src/main/java/com/martinezzf/breakbitee/ui/navegation/LocalAllItems.kt
@@ -1,7 +1,12 @@
+/**
+ * Se utiliza para compartir datos entre pantallas
+ * Se utiliza la herramienta de compositionLocalOf para hacer un contexto global temporal como variables compartidas, estados globales, dependencias u objetos comunes.
+ */
+
 package com.martinezzf.breakbitee.ui.navegation
 
 import androidx.compose.runtime.compositionLocalOf
 import com.martinezzf.breakbitee.data.UserOrderItemUi
 
-// Contexto compartido entre pantallas
+//Se comparte una lista, donde se encuentra todos los productos del pedido del usuario.
 val LocalAllItems = compositionLocalOf { mutableListOf<UserOrderItemUi>() }
