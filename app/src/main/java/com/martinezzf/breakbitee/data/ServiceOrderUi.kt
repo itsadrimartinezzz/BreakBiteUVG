bee4e9cbdb6996cfa40848c11db155c44925a117
Chon211205 <lee241203@uvg.edu.gt>
2025-11-15 22:01:12 -0600

    documentacion de ServiceOrderUi

--- app/src/main/java/com/martinezzf/breakbitee/data/ServiceOrderUi.kt
+++ app/src/main/java/com/martinezzf/breakbitee/data/ServiceOrderUi.kt
@@ -1,12 +1,15 @@
+/**
+ * Modelo que representa un pedido desde el la perspectiva del servicio
+ */
+
 package com.martinezzf.breakbitee.data
 
-data class ServiceOrderUi(
-    val id: String,
-    val estado: String,
-    val cliente: String,
-    val cantidadProductos: Int,
-    val total: String,
 
-    // 🔥 Nuevo: restaurante dueño del pedido
-    val serviceId: String
+data class ServiceOrderUi(
+    val id: String, //id del pedido
+    val estado: String, //estado del pedido
+    val cliente: String, //nombre del cliente
+    val cantidadProductos: Int, //cantidad de productos de la orden
+    val total: String, //precio total del pedido
+    val serviceId: String //id del restaurante, se utiliza en el fakeApi para identificar de que restaurante es el pedido
 )
