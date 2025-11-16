dda0d0a6e80496093de553234d883c453011d1d9
Chon211205 <lee241203@uvg.edu.gt>
2025-11-15 21:51:39 -0600

    Documentacion del OrderUi

--- app/src/main/java/com/martinezzf/breakbitee/data/OrderUi.kt
+++ app/src/main/java/com/martinezzf/breakbitee/data/OrderUi.kt
@@ -1,10 +1,17 @@
+/**
+ * Modelo que representa un pedido completo en la aplicacion.
+ * La diferenccia de OrderItemUi es que este describe cada producto dentro del pedido,
+ * OrderUi describe el pedido entero.
+ */
+
 package com.martinezzf.breakbitee.data
 
+//Representa un pedido completo que realizo el usuario en la aplicacion.
 data class OrderUi(
-    val id: String,
-    val serviceName: String,
-    val totalQ: Int,
-    val productCount: Int,
-    val dateTime: String,
-    val status: String
+    val id: String, //id del pedido
+    val serviceName: String, //Nombre del restaurante donde se hizo el pedido.
+    val totalQ: Int, //Monto total del pedido
+    val productCount: Int, //Cantidad total de productos dentro de la orden
+    val dateTime: String, //Fecha y hora que se realizo el pedido
+    val status: String //Estado del pedido.
 )
