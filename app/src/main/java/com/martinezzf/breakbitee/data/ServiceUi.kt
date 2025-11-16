2abf21fd480cf7f3801b57dbf8510f1cf0da8df8
Chon211205 <lee241203@uvg.edu.gt>
2025-11-15 22:04:08 -0600

    Documentacion de ServiceUi

--- app/src/main/java/com/martinezzf/breakbitee/data/ServiceUi.kt
+++ app/src/main/java/com/martinezzf/breakbitee/data/ServiceUi.kt
@@ -1,13 +1,17 @@
+/**
+ * Model que representa a los restaurantes en UserHomeScreen
+ */
+
 package com.martinezzf.breakbitee.data
 
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ServiceUi(
-    val id: String,
-    val name: String,
-    val imageUrl: String,
-    val bannerUrl: String,
-    val time: String
+    val id: String, //id del restaurante
+    val name: String, //nombre del restaurante
+    val imageUrl: String, //imagen de fondo del restaurante
+    val bannerUrl: String, //Logo del restaurante
+    val time: String //tiempo estimado de preparacion de productos del restaurante
 )
 
